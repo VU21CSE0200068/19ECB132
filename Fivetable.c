273865ee36be2a5390e59e3cbb31f1d2519172e6
Rajasagi varshitha <101540691+VU21CSE0200068@users.noreply.github.com>
2022-04-08 09:41:03 +0530

    Five table.c
    This program is about five table

---
+++ Fivetable.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<time.h>
+#include<limits.h>
+#include<stdint.h>
+#include<float.h>
+#include<string.h>
+#include<ctype.h>
+int fivetable(int a,int i){
+    if(i==0)
+    return 0;
+    printf("%d * %d = %d\n",a,11-i,a*(11-i));
+    fivetable(a,--i);
+}
+int main()
+{
+    int a,size;
+    a=2;
+    size=8;
+    fivetable(a,size);
+    return 0;
+}
