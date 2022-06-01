f35f7833d091923ba1876e35d95b9d45c6a4d233
VU21CSEN0200068 <101540691+VU21CSE0200068@users.noreply.github.com>
2022-06-01 09:51:44 +0530

    Update and rename palindrome.c to sumofnaturalnumbers.c

---
+++ sumofnaturalnumbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	int i,s=0,n;
+	printf("enter the integer");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		s = s + i;
+	}
+	printf("%d",s);
+}
+/* Output
+enter the integer 5
+15
+  */
