8f9882ccede019867224ceac275955285495d5e3
VU21CSEN0200068 <101540691+VU21CSE0200068@users.noreply.github.com>
2022-06-01 04:47:10 +0000

    output

--- reversingarray.c
+++ reversingarray.c
@@ -50,7 +50,8 @@ void swap(int *a, int *b)
     *a = *b;
     *b = temp;
 }
-enter the size 
+/*
+ enter the size 
 3
 Enter your elements 
 1
@@ -58,4 +59,4 @@ Enter your elements
 3
 1 2 3 
 3 2 1 
-~/19ECB132-5$ 
+*/ 
